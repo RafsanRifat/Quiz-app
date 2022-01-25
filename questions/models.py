9c47dc0f8e5beec6e2121faec88273f9f7494c95
Rafsanjani <rafsan.rifat255@gmail.com>
2022-01-25 16:45:37 +0600

    question model added

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,20 @@
+import datetime
+from django.db.models.fields import (
+    DateField, DateTimeField, DurationField, Field, IntegerField, TimeField,
+)
+from django.utils import timezone
 from django.db import models
+from quizes.models import Quiz
+
 
 # Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=120)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
+
+    def get_answer(self):
+        pass
