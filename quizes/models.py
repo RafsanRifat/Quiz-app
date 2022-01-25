2361694cbfdc677f8eee92eaa3d1fb695085ee66
Rafsanjani <rafsan.rifat255@gmail.com>
2022-01-25 11:45:00 +0600

    Quiz model added

--- quizes/models.py
+++ quizes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Quiz(models.Model):
+    DEF_CHOICE = (
+        ('easy', 'easy'),
+        ('medium', 'medium'),
+        ('hard', 'hard'),
+    )
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="duration of the quiz in minutes:")
+    required_score_to_pass = models.IntegerField(help_text="required score : ")
+    difficulty = models.CharField(max_length=6, choices=DEF_CHOICE)
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+
+    def get_question(self):
+        pass
